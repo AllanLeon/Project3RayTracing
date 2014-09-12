4bd9be082d2ff02cb17f8505219e7b35ba2f12b8
Allan Leon <allan.leon@outlook.com>
2014-09-12 16:13:49 -0400

    Updated basic classes

--- src/RayTracing/model/Ray.java
+++ src/RayTracing/model/Ray.java
@@ -2,4 +2,27 @@ package raytracing.model;
 
 public class Ray {
 
+	private Point start;
+	private Vector direction;
+	
+	public Ray(Point start, Vector direction) {
+		this.start = start;
+		this.direction = direction;
+	}
+
+	public Point getStart() {
+		return start;
+	}
+
+	public Vector getDirection() {
+		return direction;
+	}
+
+	public void setStart(Point start) {
+		this.start = start;
+	}
+
+	public void setDirection(Vector direction) {
+		this.direction = direction;
+	}
 }
--- src/RayTracing/model/Sphere.java
+++ src/RayTracing/model/Sphere.java
@@ -2,33 +2,25 @@ package raytracing.model;
 
 public class Sphere extends Object {
 
-	private int centerX, centerY, radius;
+	private Point center;
+	private int radius;
 	
-	public Sphere(int centerX, int centerY, int radius, double ka, double kd, double ks, Color color) {
+	public Sphere(Point center, int radius, double ka, double kd, double ks, Color color) {
 		super(ka, kd, ks, color);
-		this.centerX = centerX;
-		this.centerY = centerY;
+		this.center = center;
 		this.radius = radius;
 	}
 
-	public int getCenterX() {
-		return centerX;
-	}
-
-	public int getCenterY() {
-		return centerY;
+	public Point getCenter() {
+		return center;
 	}
 
 	public int getRadius() {
 		return radius;
 	}
 
-	public void setCenterX(int centerX) {
-		this.centerX = centerX;
-	}
-
-	public void setCenterY(int centerY) {
-		this.centerY = centerY;
+	public void setCenter(Point center) {
+		this.center = center;
 	}
 
 	public void setRadius(int radius) {
