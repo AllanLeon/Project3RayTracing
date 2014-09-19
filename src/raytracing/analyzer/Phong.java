d38218dfdebdcb72db907277d5982e91864b0b07
Allan Leon <allan.leon@outlook.com>
2014-09-18 21:11:01 -0400

    Solved shadows bugs

--- src/raytracing/analyzer/Phong.java
+++ src/raytracing/analyzer/Phong.java
@@ -3,6 +3,7 @@ package raytracing.analyzer;
 import java.util.List;
 
 import raytracing.model.Color;
+import raytracing.model.Ray;
 import raytracing.model.basics.Point;
 import raytracing.model.basics.Vector;
 import raytracing.model.scene.Light;
@@ -11,10 +12,10 @@ import raytracing.model.scene.Scene;
 
 public class Phong {
 
-	public static Color chromaticPhong(Point p, Object o, Scene scene) {
-		Color envComp = environmentalComponent(o, scene);
-		Color difComp = diffuseComponent(p, o, scene);
-		Color speComp = specularComponent(p, o, scene);
+	public static Color chromaticPhong(Point intersectionPoint, Object oMin, Scene scene) {
+		Color envComp = environmentalComponent(oMin, scene);
+		Color difComp = diffuseComponent(intersectionPoint, oMin, scene);
+		Color speComp = specularComponent(intersectionPoint, oMin, scene);
 		double r = envComp.getR() + difComp.getR() + speComp.getR();
 		double g = envComp.getG() + difComp.getG() + speComp.getG();
 		double b = envComp.getB() + difComp.getB() + speComp.getB();
@@ -23,9 +24,9 @@ public class Phong {
 		return phong;
 	}
 	
-	public static Color environmentalComponent(Object o, Scene scene) {
+	public static Color environmentalComponent(Object oMin, Scene scene) {
 		Color ia = scene.getEnvironmentalLightColor();
-		Color objectColor = o.getColor();
+		Color objectColor = oMin.getColor();
 		double ka = scene.getEnvironmentalCoef();
 		double r = ia.getR() * ka * objectColor.getR();
 		double g = ia.getG() * ka * objectColor.getG();
@@ -33,65 +34,84 @@ public class Phong {
 		return new Color(r, g, b);
 	}
 	
-	private static Color diffuseComponent(Point p, Object o, Scene scene) {
+	private static Color diffuseComponent(Point intersectionPoint, Object oMin, Scene scene) {
 		List<Light> lights = scene.getLights();
-		Vector normal = o.getNormal(p);
-		Color objectColor = o.getColor();
+		Vector normal = oMin.getNormal(intersectionPoint);
+		Color objectColor = oMin.getColor();
 		double r = 0;
 		double g = 0;
 		double b = 0;
 		for (int i = 0; i < lights.size(); i++) {
 			Light light = lights.get(i);
 			Color lightColor = light.getColor();
-			double dotP = Vector.dotProduct(normal, light.getNormalizedDirection(p));
+			double dotP = Vector.dotProduct(normal, light.getNormalizedDirection(intersectionPoint));
 			if (dotP > 0) {
-				double attenuationFactor = calculateAttenuationFactor(light, p);
-				r += lightColor.getR()*dotP * attenuationFactor;
-				g += lightColor.getG()*dotP * attenuationFactor;
-				b += lightColor.getB()*dotP * attenuationFactor;
+				if (!checkShadowRayIntersection(light, intersectionPoint, scene, oMin)) {
+					double attenuationFactor = calculateAttenuationFactor(light, intersectionPoint);
+					r += lightColor.getR()*dotP * attenuationFactor;
+					g += lightColor.getG()*dotP * attenuationFactor;
+					b += lightColor.getB()*dotP * attenuationFactor;
+				}
 			}
 		}
-		r *= o.getKd() * objectColor.getR();
-		g *= o.getKd() * objectColor.getG();
-		b *= o.getKd() * objectColor.getB();
+		r *= oMin.getKd() * objectColor.getR();
+		g *= oMin.getKd() * objectColor.getG();
+		b *= oMin.getKd() * objectColor.getB();
 		return new Color(r, g, b);
 	}
 	
-	private static Color specularComponent(Point p, Object o, Scene scene) {
+	private static Color specularComponent(Point intersectionPoint, Object oMin, Scene scene) {
 		List<Light> lights = scene.getLights();
-		Vector normal = o.getNormal(p);
-		double vx = 0 - p.getX();
-		double vy = 0 - p.getY();
-		double vz = -100 - p.getZ();
+		Vector normal = oMin.getNormal(intersectionPoint);
+		double vx = 0 - intersectionPoint.getX();
+		double vy = 0 - intersectionPoint.getY();
+		double vz = -100 - intersectionPoint.getZ();
 		Vector obsVec = new Vector(vx, vy, vz);
 		obsVec.normalize();
 		
 		double r = 0;
 		double g = 0;
 		double b = 0;
-		int n = o.getN();
+		int n = oMin.getN();
 		for (int i = 0; i < lights.size(); i++) {
 			Light light = lights.get(i);
 			Color lightColor = light.getColor();
-			Vector l = Vector.invert(light.getNormalizedDirection(p));
+			Vector l = Vector.invert(light.getNormalizedDirection(intersectionPoint));
 			Vector reflection = GeometricAnalyzer.perfectSpecularReflection(l, normal);
 			double dotP = Vector.dotProduct(obsVec, reflection);
 			if (dotP > 0) {
-				double attenuationFactor = calculateAttenuationFactor(light, p);
-				r += lightColor.getR()*Math.pow(dotP, n) * attenuationFactor;
-				g += lightColor.getG()*Math.pow(dotP, n) * attenuationFactor;
-				b += lightColor.getB()*Math.pow(dotP, n) * attenuationFactor;
+				if (!checkShadowRayIntersection(light, intersectionPoint, scene, oMin)) {
+					double attenuationFactor = calculateAttenuationFactor(light, intersectionPoint);
+					r += lightColor.getR()*Math.pow(dotP, n) * attenuationFactor;
+					g += lightColor.getG()*Math.pow(dotP, n) * attenuationFactor;
+					b += lightColor.getB()*Math.pow(dotP, n) * attenuationFactor;
+				}
 			}
 		}
-		r *= o.getKs();
-		g *= o.getKs();
-		b *= o.getKs();
+		r *= oMin.getKs();
+		g *= oMin.getKs();
+		b *= oMin.getKs();
 		return new Color(r, g, b);
 	}
 
 	private static double calculateAttenuationFactor(Light light, Point p) {
 		double distance = p.getDistanceTo(light.getPosition());
-		double attenuationFactor = 1 / (0.05 + 0.001 * distance + 0.000001 * Math.pow(distance, 2));
+		double attenuationFactor = 1 / (0.005 + 0.001 * distance + 0.000001 * Math.pow(distance, 2));
 		return Math.min(attenuationFactor, 1);
+	}
+	
+	private static boolean checkShadowRayIntersection(Light light, Point intersectionPoint, Scene scene, Object oMin) {
+		for (int j = 0; j < scene.getObjects().size(); j++) {
+			Object object = scene.getObjects().get(j);
+			if (oMin != object) {
+				Vector direction = light.getDirection(intersectionPoint);
+				Ray shadowRay = new Ray(intersectionPoint, direction);
+				double t = object.checkIntersection(shadowRay);
+				if (t >= 0 && t <= 1) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
--- src/raytracing/model/scene/SceneFactory.java
+++ src/raytracing/model/scene/SceneFactory.java
@@ -27,12 +27,12 @@ public class SceneFactory {
 	}
 	
 	public static Scene createSpiralScene() {
+		Random rand = new Random();
 		Color lightColor = new Color(1, 1, 1);
 		Scene scene = new Scene(lightColor, 0.8);
-		Light light1 = new Light(lightColor, new Point(-800, -910, 200));
+		/*Light light1 = new Light(lightColor, new Point(-800, -910, 200));
 		Light light2 = new Light(lightColor, new Point(0, -800, 500));
 		Light light3 = new Light(lightColor, new Point(400, -800, 700));
-		Random rand = new Random();
 		for (int i = 0; i <= 5; i++) {
 			for (int k = 0; k <= 5; k++) {
 				double sx = -1500 + 600*i;
@@ -47,41 +47,36 @@ public class SceneFactory {
 			}
 			Light light = new Light(lightColor, new Point(-1500+600*i -100, -700, 400));
 			scene.addLight(light);
-		}
-		for (int i = 0; i < 50; i++) {
+		}*/
+		for (int i = 0; i < 60; i++) {
 			double sx = 600*Math.cos(i);
 			double sy = 600*Math.sin(i);
-			double sz = 400 + i*20;
-			double lx = 400*Math.cos(i);
-			double ly = 400*Math.sin(i);
-			double lz = 400 + i*20;
+			double sz = 300 + i*20;
+			
 			double r = rand.nextDouble();
 			double g = rand.nextDouble();
 			double b = rand.nextDouble();
 			Object sphere = new Sphere(new Point(sx, sy, sz), 80, new Color(r, g, b), 0.7, 1, 7, false);
 			scene.addObject(sphere);
+			double lx = 400*Math.cos(i);
+			double ly = 400*Math.sin(i);
+			double lz = 300 + i*20;
 			Light light = new Light(lightColor, new Point(lx, ly, lz));
 			scene.addLight(light);
 		}
-		
-		/*for (int i = 0; i < 50; i++) {
-			double x = rand.nextInt(500) - 1000;
-			double y = rand.nextInt(500) - 1000;
-			double z = rand.nextInt(800) - 400;
+		/*
+		for (int i = 0; i < 20; i++) {
+			double x  = rand.nextDouble() * 1000 - 500;
+			double y = rand.nextDouble() * 1000 - 500;
+			double z = rand.nextDouble() * 400 + 200;
+			System.out.println(x + " " + y + " " + z);
 			Light light = new Light(lightColor, new Point(x, y, z));
 			scene.addLight(light);
 		}*/
-		Object sphere1 = new Sphere(new Point(0, 0, 1000), 300, new Color(0.2, 0.2, 0.2), 0.4, 1, 7, true);
-		/*Object sphere2 = new Sphere(new Point(-40, 40, 300), 40, new Color(1, 0, 0), 0.4, 1, 7, true);
-		Object sphere3 = new Sphere(new Point(-80, 80, 100), 50, new Color(0, 1, 0), 0.4, 1, 7);
-		scene.addLight(light1);
-		scene.addLight(light2);*/
+		Object sphere1 = new Sphere(new Point(0, 0, 1000), 300,
+				new Color(rand.nextDouble(), rand.nextDouble(), rand.nextDouble()),
+				0.4, 1, 7, true);
 		scene.addObject(sphere1);
-		//scene.addObject(sphere2);
-		//scene.addLight(light1);
-		//scene.addLight(light2);
-		//scene.addLight(light3);
-		//scene.addObject(sphere3);
 		return scene;
 	}
 }
